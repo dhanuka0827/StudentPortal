eaf85fe3d11e453915c9faf97fc79defca22d414
dhanuka0827 <dhanuka0827@gmail.com>
2024-03-19 18:56:25 +0530

    Initial core logic

--- src/main/java/com/uni/portal/controller/StudentController.java
+++ src/main/java/com/uni/portal/controller/StudentController.java
@@ -5,9 +5,7 @@ import com.uni.portal.service.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,18 +18,18 @@ public class StudentController {
     StudentService service;
 
     @PostMapping("/add")
-    public void addStudent(Student student){
+    public void addStudent(@RequestBody Student student){
         logger.info("Adding student to db : ", student);
         service.save(student);
     }
 
-    @RequestMapping("/get/{id}")
-    public Student getStudent(int id){
+    @GetMapping("/get/{id}")
+    public Student getStudent(@PathVariable int id){
         logger.info("Getting student with id : ", id);
         return service.getById(id);
     }
 
-    @RequestMapping("/getAll")
+    @GetMapping("/getAll")
     public List<Student> getAllStudents(){
         logger.info("Getting all students");
         return service.getAll();
