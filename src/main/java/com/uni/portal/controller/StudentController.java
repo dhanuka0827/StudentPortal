d6bec92ca75b85a1f1d24d2345a8bfb321fbfae2
Chitrendra Chaudhary <chitrendra.chaudhary@irdeto.com>
2024-03-20 16:32:39 +0530

    added one API for HQL select

--- src/main/java/com/uni/portal/controller/StudentController.java
+++ src/main/java/com/uni/portal/controller/StudentController.java
@@ -1,37 +1,48 @@
 package com.uni.portal.controller;
 
-import com.uni.portal.entity.Student;
-import com.uni.portal.service.StudentService;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.uni.portal.entity.Student;
+import com.uni.portal.service.StudentService;
 
 @RestController("/student")
 public class StudentController {
 
-    Logger logger = LoggerFactory.getLogger(StudentController.class);
+	Logger logger = LoggerFactory.getLogger(StudentController.class);
 
-    @Autowired
-    StudentService service;
+	@Autowired
+	StudentService service;
 
-    @PostMapping("/add")
-    public void addStudent(@RequestBody Student student){
-        logger.info("Adding student to db : ", student);
-        service.save(student);
-    }
+	@PostMapping("/add")
+	public void addStudent(@RequestBody Student student) {
+		logger.info("Adding student to db : ", student);
+		service.save(student);
+	}
 
-    @GetMapping("/get/{id}")
-    public Student getStudent(@PathVariable int id){
-        logger.info("Getting student with id : ", id);
-        return service.getById(id);
-    }
+	@GetMapping("/get/{id}")
+	public Student getStudent(@PathVariable int id) {
+		logger.info("Getting student with id : ", id);
+		return service.getById(id);
+	}
 
-    @GetMapping("/getAll")
-    public List<Student> getAllStudents(){
-        logger.info("Getting all students");
-        return service.getAll();
-    }
+	@GetMapping("/getALLByQuery/{id}")
+	public List<Student> getALLByQuery() {
+		logger.info("Loading student from query");
+		return service.getByQuery();
+	}
+
+	@GetMapping("/getAll")
+	public List<Student> getAllStudents() {
+		logger.info("Getting all students");
+		return service.getAll();
+	}
 }
